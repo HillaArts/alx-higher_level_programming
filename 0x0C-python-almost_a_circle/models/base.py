fca42be1801b18a7f792c180a75db13349657a4f
HillaArts <hassenyehillary@gmail.com>
2023-12-14 00:11:28 -0800

    update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Module defining the Base class."""
 
+import json
+
+
 class Base:
-    """Base class for managing id attribute."""
+    """Base class for other classes."""
 
     __nb_objects = 0
 
@@ -10,28 +13,127 @@ class Base:
         """Initialize a new instance of Base.
 
         Args:
-            id (int): The identifier for the instance. If not provided,
-                      increment the class attribute __nb_objects and assign
-                      it to the public instance attribute id.
+            id (int): Identifier for the instance (default is None).
         """
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): List of instances.
+
+        """
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of instances represented by json_string.
+
+        Args:
+            json_string (str): JSON string representing a list of dictionaries.
+
+        Returns:
+            list: List of dictionaries.
+        """
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set.
+
+        Args:
+            dictionary (dict): Dictionary with attributes.
+
+        Returns:
+            instance: Instance with attributes set.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            return None
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances.
+
+        Returns:
+            list: List of instances.
+        """
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, "r") as file:
+                json_data = file.read()
+                list_dicts = cls.from_json_string(json_data)
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
+
 
 if __name__ == "__main__":
-    b1 = Base()
-    print(b1.id)
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    list_rectangles_input = [r1, r2]
 
-    b2 = Base()
-    print(b2.id)
+    Rectangle.save_to_file(list_rectangles_input)
 
-    b3 = Base()
-    print(b3.id)
+    list_rectangles_output = Rectangle.load_from_file()
 
-    b4 = Base(12)
-    print(b4.id)
+    for rect in list_rectangles_input:
+        print("[{}] {}".format(id(rect), rect))
 
-    b5 = Base()
-    print(b5.id)
+    print("---")
+
+    for rect in list_rectangles_output:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+    print("---")
+
+    s1 = Square(5)
+    s2 = Square(7, 9, 1)
+    list_squares_input = [s1, s2]
+
+    Square.save_to_file(list_squares_input)
+
+    list_squares_output = Square.load_from_file()
+
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
